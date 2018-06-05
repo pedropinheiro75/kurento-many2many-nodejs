a727d3cee7ef8ae502ca97bc52133039f6fc9fb0
Gnosis <gnosis7bit@gmail.com>
2018-06-05 15:35:57 -0300

    docker support with docker-compose file

---
+++ docker/kurento-many2many/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+cd /src
+
+if [ -d "$node_modules" ]; then
+    rm -r node_modules
+fi
+npm cache clean --force
+npm install
+cd static
+bower install --allow-root
+
+until npm start -- --ws_uri=ws://${KURENTO_MEDIA_SERVER_HOST}:${KURENTO_MEDIA_SERVER_PORT}/kurento; do
+    echo "Server 'kurento-one2many-nodejs' crashed with exit code $?.  Respawning.." >&2
+    sleep 1
+done
